3469de8854e644d3b9ae814bdce4596f30878377
EX3 <inthe_6788@naver.com>
2024-10-01 14:30:26 +0900

    Fixed appveyor

---
+++ appveyor.py
@@ -0,0 +1,81 @@
+import os
+import sys
+from datetime import datetime
+
+appcast_ver = os.environ.get('APPVEYOR_BUILD_VERSION')
+
+
+def write_appcast(appcast_os, appcast_rid, appcast_file):
+
+    xml = '''<?xml version="1.0" encoding="utf-8"?>
+<rss version="2.0" xmlns:sparkle="http://www.andymatuschak.org/xml-namespaces/sparkle">
+<channel>
+    <title>MiriVoice</title>
+    <language>en</language>
+    <item>
+    <title>MiriVoice %s</title>
+    <pubDate>%s</pubDate>
+    <enclosure url="https://github.com/EX3exp/MiriVoice/releases/download/build%%2F%s/%s"
+                sparkle:version="%s"
+                sparkle:shortVersionString="%s"
+                sparkle:os="%s"
+                type="application/octet-stream"
+                sparkle:signature="" />
+    </item>
+</channel>
+</rss>''' % (appcast_ver, datetime.now().strftime("%a, %d %b %Y %H:%M:%S %z"),
+             appcast_ver, appcast_file, appcast_ver, appcast_ver, appcast_os)
+
+    with open("appcast.%s.xml" % (appcast_rid), 'w') as f:
+        f.write(xml)
+
+
+if sys.platform == 'win32':
+    if appcast_ver is not None:
+        os.system("git tag build/%s 2>&1" % (appcast_ver))
+        os.system("git push origin build/%s 2>&1" % (appcast_ver))
+
+    os.system("del *.xml 2>&1")
+
+    os.system("dotnet restore Mirivoice.Desktop/Mirivoice.Desktop.csproj -r win-x86")
+    os.system(
+        "dotnet publish Mirivoice.Desktop/Mirivoice.Desktop.csproj -c Release -r win-x86 --self-contained true -o bin/win-x86")
+    write_appcast("windows", "win-x86", "MiriVoice-win-x86.zip")
+
+    os.system("dotnet restore Mirivoice.Desktop/Mirivoice.Desktop.csproj -r win-x64")
+    os.system(
+        "dotnet publish Mirivoice.Desktop/Mirivoice.Desktop.csproj -c Release -r win-x64 --self-contained true -o bin/win-x64")
+    write_appcast("windows", "win-x64", "MiriVoice-win-x64.zip")
+
+
+elif sys.platform == 'darwin':
+    os.system("rm *.dmg")
+    os.system("rm *.xml")
+
+    os.system("git checkout Mirivoice.Desktop/Mirivoice.Desktop.csproj")
+    os.system("rm LICENSE.txt")
+    os.system(
+        "sed -i '' \"s/0.0.0/%s/g\" Mirivoice.Desktop/Mirivoice.Desktop.csproj" % (appcast_ver))
+    os.system("dotnet restore Mirivoice.Desktop/Mirivoice.Desktop.csproj -r osx.10.14-x64")
+    os.system("dotnet msbuild Mirivoice.Desktop/Mirivoice.Desktop.csproj -t:BundleApp -p:Configuration=Release -p:RuntimeIdentifier=osx.10.14-x64 -p:UseAppHost=true -p:OutputPath=../bin/osx-x64/")
+    os.system(
+        "cp MiriVoice/Assets/mirivoice.icns bin/osx-x64/publish/MiriVoice.app/Contents/Resources/")
+    os.system("rm *.dmg")
+    os.system("npm install -g create-dmg")
+    os.system("create-dmg bin/osx-x64/publish/MiriVoice.app")
+    os.system("mv *.dmg MiriVoice-osx-x64.dmg")
+    os.system("codesign -fvs - MiriVoice-osx-x64.dmg")
+    os.system("git checkout Mirivoice.Desktop/Mirivoice.Desktop.csproj")
+    os.system("git checkout LICENSE.txt")
+
+    write_appcast("macos", "osx-x64", "MiriVoice-osx-x64.dmg")
+
+else:
+    os.system("rm *.xml")
+
+    os.system("dotnet restore Mirivoice.Desktop/Mirivoice.Desktop.csproj -r linux-x64")
+    os.system(
+        "dotnet publish Mirivoice.Desktop/Mirivoice.Desktop.csproj -c Release -r linux-x64 --self-contained true -o bin/linux-x64")
+    os.system("chmod +x bin/linux-x64/MiriVoice")
+    os.system("tar -C bin/linux-x64 -czvf MiriVoice-linux-x64.tar.gz .")
+    write_appcast("linux", "linux-x64", "MiriVoice-linux-x64.tar.gz")
