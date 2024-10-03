2b64c96cd6a94677e5cd5cce8bb4576256a987cb
EX3 <inthe_6788@naver.com>
2024-10-03 10:27:42 +0900

    Remove dotnet bundle

--- Mirivoice.Desktop/appveyor.py
+++ Mirivoice.Desktop/appveyor.py
@@ -29,6 +29,38 @@ def write_appcast(appcast_os, appcast_rid, appcast_file):
     with open("appcast.%s.xml" % (appcast_rid), 'w') as f:
         f.write(xml)
 
+def write_info_plist():
+    plist = '''<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+  <dict>
+    <key>CFBundleName</key>
+    <string>MiriVoice</string>
+    <key>CFBundleDisplayName</key>
+    <string>MiriVoice</string>
+    <key>CFBundleIdentifier</key>
+    <string>com.ex3.mirivoice</string>
+    <key>CFBundleVersion</key>
+    <string>@@</string>
+    <key>CFBundlePackageType</key>
+    <string>APPL</string>
+    <key>CFBundleSignature</key>
+    <string>????</string>
+    <key>CFBundleExecutable</key>
+    <string>MiriVoice</string>
+    <key>CFBundleIconFile</key>
+    <string>mirivoice.icns</string>
+    <key>CFBundleShortVersionString</key>
+    <string>@@</string>
+    <key>NSPrincipalClass</key>
+    <string>NSApplication</string>
+    <key>NSHighResolutionCapable</key>
+    <true />
+  </dict>
+</plist>'''.replace('@@', appcast_ver)
+    
+    with open("Mirivoice.Desktop/osxbuild/MiriVoice.app/Contents/Info.plist", 'w') as f:
+        f.write(plist)
 
 if sys.platform == 'win32':
     if appcast_ver is not None:
@@ -53,20 +85,17 @@ elif sys.platform == 'darwin':
     os.system("rm *.xml")
 
     os.system("git checkout Mirivoice.Desktop/Mirivoice.Desktop.csproj")
-    os.system("rm LICENSE.txt")
     os.system(
         "sed -i '' \"s/0.0.0/%s/g\" Mirivoice.Desktop/Mirivoice.Desktop.csproj" % (appcast_ver))
+    write_info_plist()
     os.system("dotnet restore Mirivoice.Desktop/Mirivoice.Desktop.csproj -r osx-x64")
-    os.system("dotnet msbuild Mirivoice.Desktop/Mirivoice.Desktop.csproj -t:BundleApp -p:Configuration=Release -p:RuntimeIdentifier=osx-x64 -p:UseAppHost=true -p:OutputPath=../Mirivoice.Desktop/bin/osx-x64/ -p:AssemblyVersion=%s" % (appcast_ver))
-    os.system(
-        "cp Mirivoice.Desktop/mirivoice.icns Mirivoice.Desktop/bin/osx-x64/publish/MiriVoice.app/Contents/Resources/")
-    os.system("rm *.dmg")
+    os.system("dotnet publish Mirivoice.Desktop/Mirivoice.Desktop.csproj -c Release -r osx-x64 --self-contained true -o Mirivoice.Desktop/bin/osx-x64 -p:UseAppHost=true -p:AssemblyVersion=%s" % (appcast_ver) )
+    os.system("cp -a  Mirivoice.Desktop/bin/osx-x64/publish/ Mirivoice.Desktop/osxbuild/MiriVoice.app/Contents/MacOS")    
+    os.system("chmod +x Mirivoice.Desktop/osxbuild/MiriVoice.app/Contents/MacOS/MiriVoice")
     os.system("npm install -g create-dmg")
-    os.system("create-dmg Mirivoice.Desktop/bin/osx-x64/publish/MiriVoice.app")
+    os.system("create-dmg Mirivoice.Desktop/osxbuild/MiriVoice.app")
     os.system("mv *.dmg MiriVoice-osx-x64.dmg")
-    os.system("codesign -fvs - MiriVoice-osx-x64.dmg")
-    os.system("git checkout Mirivoice.Desktop.csproj")
-    os.system("git checkout LICENSE.txt")
+    os.system("git checkout Mirivoice.Desktop/Mirivoice.Desktop.csproj")
 
     write_appcast("macos", "osx-x64", "MiriVoice-osx-x64.dmg")
 
